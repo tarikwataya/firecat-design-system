f644452277c0937996af05a794a4e3c39523dfcc
Tárik Wataya <tarik.wataya@pixter.com.br>
2023-08-09 19:15:21 -0300

    fix: Add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -27,7 +27,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/3scorp-ui-system/'
+      config.base = '/firecat-design-system/'
     }
 
     return config
